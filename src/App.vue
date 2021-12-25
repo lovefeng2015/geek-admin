e6b963c6d5ba37c5d8fe44525bd9736fcfcd6387
yhf286_glb <yhf286_glb@qq.com>
2021-12-25 20:35:21 +0800

    vue3 第一项目

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="你好，极客时间" />
+</template> -->
+<template>
+  <div>
+    <router-link to="/">首页</router-link> | 
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
