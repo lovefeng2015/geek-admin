d7d32e92bcd0c52b06dc1e0b8a424867b0b7cd3e
lovefeng2015 <yhg28754889@gmail.com>
2021-12-27 22:07:37 +0800

    rate commponse complete

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -1,8 +1,21 @@
 <template>
-    <div :style="fontstyle">{{ rate }}</div>
+    <div :style="fontstyle">
+        <div class="rate" @mouseout="mouseOut">
+            <span class="star" @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+            <span class="hollow" :style="fontwidth">
+                <span
+                    class="star"
+                    @click="onRate(num)"
+                    @mouseover="mouseOver(num)"
+                    v-for="num in 5"
+                    :key="num"
+                >★</span>
+            </span>
+        </div>
+    </div>
 </template>
 <script setup>
-import { computed, defineProps } from 'vue';
+import { computed, defineProps, ref, defineEmits } from 'vue';
 let props = defineProps({
     value: Number,
     theme: { type: String, default: 'orange' }
@@ -13,4 +26,35 @@ const themeObj = { 'black': '#00', 'white': '#fff', 'red': '#f5222d', 'orange': 
 const fontstyle = computed(() => {
     return `color:${themeObj[props.theme]}`;
 })
+let width = ref(props.value);
+function mouseOver(i) {
+    width.value = i;
+}
+function mouseOut() {
+    width.value = props.value;
+}
+const fontwidth = computed(() => `width:${width.value}em;`)
+//let emits=defineEmits('[update-rate]');
+let emits = defineEmits(['update-rate']);
+function onRate(num) { emits('update-rate', num) }
 </script>
+<style scoped>
+.rate {
+    position: relative;
+    display: inline-block;
+}
+.rate > span.hollow {
+    position: absolute;
+    display: inline-block;
+    top: 0;
+    left: 0;
+    width: 0;
+    overflow: hidden;
+
+    white-space: nowrap;
+}
+.rate .star {
+    display: inline-block;
+    width: 1em;
+}
+</style>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,15 +1,18 @@
 
 <template>
     <h1>这是关于页面</h1>
-    <Rate :value="score"></Rate>
-    <Rate :value="2"></Rate>
-    <Rate :value="4" theme="red"></Rate>
-    <Rate :value="1" theme="green"></Rate>
+    <!-- <Rate :value="score"></Rate> -->
+    <!-- <Rate :value="3.5"></Rate> -->
+    <!-- <Rate :value="4" theme="red"></Rate>
+    <Rate :value="1" theme="green"></Rate>-->
+    <h2>你的评分是{{ score }}</h2>
+    <Rate :value="score" @update-rate="update"></Rate>
 </template>
 
 
 <script setup>
 import { ref } from 'vue'
 import Rate from '../components/Rate.vue'
-let score = ref(3)
+let score = ref(3.5);
+function update(num) { score.value = num }
 </script>
