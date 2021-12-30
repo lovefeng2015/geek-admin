0c3ff5bc7cb50524364122bcfb7ae4036ae86f11
lovefeng2015 <yhg28754889@gmail.com>
2021-12-30 19:49:18 +0800

    add router

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,17 +1,31 @@
 
 <template>
 <div @click="add">
-    {{count}}
+    {{count}}*2={{double}}
 </div>
+<div @click="asyncAdd">asyncAdd</div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import {useStore} from 'vuex'
-let store = useStore()
-let count = computed(()=>store.state.count)
+// import { computed } from 'vue'
+// import {useStore} from 'vuex'
+// let store = useStore()
+// let count = computed(()=>store.state.count)
 
+// function add(){
+//     store.commit('add')
+// }
+ import { computed } from 'vue'
+//import {useStore} from '../store/gvuex'
+import {useStore} from 'vuex'
+let store =useStore()
+let count = computed(()=>store.state.count);
+let double = computed(()=>store.getters.double)
 function add(){
     store.commit('add')
 }
+function asyncAdd() {
+    store.dispatch('asyncAdd');
+}
+
 </script>
--- src/store/gvuex.js
+++ src/store/gvuex.js
@@ -1,19 +1,36 @@
-
-import { inject, reactive } from 'vue'
+import {
+    inject,
+    reactive
+} from 'vue'
 
 const STORE_KEY = '__store__'
+
 function useStore() {
-  return inject(STORE_KEY)
+    return inject(STORE_KEY)
 }
+
 function createStore(options) {
-  return new Store(options)
+    return new Store(options)
 }
 class Store {
-  constructor(options) {
-    this._state = reactive({
-      data: options.state()
-    })
-    this._mutations = options.mutations
-  }
+    constructor(options) {
+        this._state = reactive({
+            data: options.state()
+        })
+        this._mutations = options.mutations
+    }
+    install(app) {
+        app.provide(STORE_KEY, this)
+    }
+    get state() {
+        return this._state.data
+    }
+    commit = (type, payload) => {
+        const entry = this._mutations[type]
+        entry && entry(this.state, payload)
+    }
 }
-export { createStore, useStore }
+export {
+    createStore,
+    useStore
+}
