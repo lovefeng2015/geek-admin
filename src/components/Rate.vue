0c2e57387df1998d1e1cdfa4118e3261f1af503e
lovefeng2015 <yhg28754889@gmail.com>
2022-03-27 21:11:08 +0800

    测试eslint

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -23,7 +23,7 @@ let props = defineProps({
     theme: { type: String, default: 'orange' }
 })
 //console.log(props);
-let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value));
+// let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value));
 const themeObj = { 'black': '#00', 'white': '#fff', 'red': '#f5222d', 'orange': '#fa541c', 'yellow': '#fadb14', 'green': '#73d13d', 'blue': '#40a9ff', }
 const fontstyle = computed(() => {
     return `color:${themeObj[props.theme]}`;
